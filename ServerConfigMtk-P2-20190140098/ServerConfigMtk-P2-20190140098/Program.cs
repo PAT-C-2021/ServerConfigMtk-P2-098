6b637a766cc12010bc80cb7e6d533bdef00d7d3d
mfariza2111 <79901687+mfariza2111@users.noreply.github.com>
2021-11-01 19:16:32 +0700

    ServerConfigMtk-P2-098

---
+++ ServerConfigMtk-P2-20190140098/ServerConfigMtk-P2-20190140098/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMat_P1_20190140098;
+
+namespace ServerConfigMtk_P2_20190140098
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
